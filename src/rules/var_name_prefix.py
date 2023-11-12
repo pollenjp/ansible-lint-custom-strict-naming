118ab628b975eb116fb48037261092ea9e94be10
pollenjp <polleninjp@gmail.com>
2023-11-12 20:47:29 +0900

    refactoring

--- src/rules/var_name_prefix.py
+++ src/rules/var_name_prefix.py
@@ -12,6 +12,8 @@ from ansiblelint.utils import Task
 from ansible_lint_custom_strict_naming import StrictFileType
 from ansible_lint_custom_strict_naming import base_name
 from ansible_lint_custom_strict_naming import detect_strict_file_type
+from ansible_lint_custom_strict_naming import get_role_name_from_role_tasks_file
+from ansible_lint_custom_strict_naming import get_tasks_name_from_tasks_file
 
 logger = getLogger(__name__)
 logger.addHandler(NullHandler())
@@ -60,19 +62,19 @@ def match_task_for_set_fact_module(task: Task, file: Lintable | None = None) -> 
 
 def match_tasks_for_role_tasks_file(task: Task, file: Lintable) -> str | None:
     # roles/<role_name>/tasks/<some_tasks>.yml
-    prefix_format = f"{file.path.parents[1].name}_role__"
+    prefix_format = f"{get_role_name_from_role_tasks_file(file)}_role__"
     for key in task.args.keys():
         if not key.startswith(prefix_format):
-            return f"Variables in role should have a `{prefix_format}` prefix."
+            return f"Variables in role should have a '{prefix_format}' prefix."
     return None
 
 
 def match_tasks_for_tasks_file(task: Task, file: Lintable) -> str | None:
     # <not_roles>/**/tasks/<some_tasks>.yml
-    prefix_format = f"{file.path.stem}_tasks__"
+    prefix_format = f"{get_tasks_name_from_tasks_file(file)}_tasks__"
     for key in task.args.keys():
         if not key.startswith(prefix_format):
-            return f"Variables in role should have a `{prefix_format}` prefix."
+            return f"Variables in tasks should have a '{prefix_format}' prefix."
     return None
 
 
@@ -88,7 +90,7 @@ def match_task_for_include_role_module(task: Task, file: Lintable | None = None)
     prefix = f"{role_name}_role__arg__"
     for key in task_vars.keys():
         if not key.startswith(f"{prefix}"):
-            return f"Variables in role should have a `{prefix}` prefix."
+            return f"Variables in 'include_role' should have a '{prefix}' prefix."
     return False
 
 
@@ -104,5 +106,5 @@ def match_task_for_include_tasks_module(task: Task, file: Lintable | None = None
     prefix = f"{role_name}_tasks__arg__"
     for key in task_vars.keys():
         if not key.startswith(f"{prefix}"):
-            return f"Variables in tasks should have a `{prefix}` prefix."
+            return f"Variables in 'include_tasks' should have a '{prefix}' prefix."
     return False
